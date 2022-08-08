464df84a45c2caea48bcc816aed0099ee096693b
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-08 13:35:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 window.addEventListener("DOMContentLoaded", function () {
     let nameField = document.querySelector(".name>span");
-    let nameFieldChildren = nameField.children;
-    
-    nameFieldChildren[2].innerHTML += "<h1>Hello</h1>";
-    
-    
     console.log(nameField);
-    console.log(nameFieldChildren);
+//     let nameFieldChildren = nameField.children;
+    
+//     nameFieldChildren[2].innerHTML += "<h1>Hello</h1>";
+    
+    
+//     console.log(nameField);
+//     console.log(nameFieldChildren);
 });
