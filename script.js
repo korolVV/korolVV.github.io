311eb6c5074dc94c276ddd40bdaf8d4e46d74bf1
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-09 20:05:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -33,15 +33,12 @@ emailField.children[1].style.cssText = 'pointer-events: none;position: absolute;
 	
 countryField.children[1].style.cssText = 'pointer-events: none;position: absolute;left: 51px;top: 23px;-webkit-transition: 0.2s;-o-transition: 0.2s;transition: 0.2s;-webkit-transition-timing-function: ease;-o-transition-timing-function: ease;transition-timing-function: ease;-webkit-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);-o-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);font-weight: 600;font-size: 16px;line-height: 15px;color: #9cabc7;-webkit-transition: 0.2s all ease-out;-o-transition: 0.2s all ease-out;transition: 0.2s all ease-out';
 	
+countryField.children[0].style.cssText = 'background-image: url(https://sunbirdapp.com/icons/country.svg);width: 14px;height: 16px';
+	
 console.log(firstNameInput);
 
-const firstNameChildren = firstName.children;
 
-const countryFieldChildren = countryField.children;
-	
-console.log(countryFieldChildren[0]);
 
-countryFieldChildren[0].style.cssText = 'background-image: url(https://sunbirdapp.com/icons/country.svg);width: 14px;height: 16px';
 
 	
 firstNameInput.addEventListener("change", (e) => {
