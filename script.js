08bd2fb4b21a025b3accfef219b6457b975822d0
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-09 14:22:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,15 @@ const lastName = nameFieldChildren[3];
 firstName.innerHTML += "<span>First name</span><img src=\"https://sunbirdapp.com/icons/name.svg\" alt=\"First name\" />";
 lastName.innerHTML += "<span>Last name</span><img src=\"https://sunbirdapp.com/icons/name.svg\" alt=\"Last name\" />";
 emailField.innerHTML += "<span>Email</span><img src=\"https://sunbirdapp.com/icons/email.svg\" alt=\"Email\" />";
-countryField.innerHTML = "<div class="country__img"></div><span>Country</span><span class="placeholder">Please select...</span><div class="country-list"><div class="country-list__search"><img src="icons/search.svg" alt="Search" /><input type="text" placeholder="Primamy Label" /></div>
+countryField.innerHTML = "
+	<div class="country__img">
+	</div><span>Country</span>
+	<span class="placeholder">Please select...</span>
+	<div class="country-list">
+		<div class="country-list__search">
+			<img src=\"icons/search.svg\" alt=\"Search\" />
+			<input type="text" placeholder="Primamy Label" />
+		</div>
 <ul>
 <li class="country-list-card">
 	<img src=\"https://sunbirdapp.com/icons/icons/flags/afganistan.svg\" alt=\"afganistan\"/><span>Afganistan</span>
