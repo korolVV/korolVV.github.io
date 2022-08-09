893d9639c851e55748d38456e5c9ccc0bde88d7f
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-09 13:58:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,10 +13,117 @@ const nameFieldChildren = nameField.children;
 const firstName = nameFieldChildren[2];
 const lastName = nameFieldChildren[3];
 
-firstName.innerHTML += "<span>First name</span>";
-lastName.innerHTML += "<span>Last name</span>";
-emailField.innerHTML += "<span>Email</span>";
-countryField.innerHTML += "<span>Country</span>";
+firstName.innerHTML += "<span>First name</span><img src="https://sunbirdapp.com/icons/name.svg" alt="First name" />";
+lastName.innerHTML += "<span>Last name</span><img src="https://sunbirdapp.com/icons/name.svg" alt="Last name" />";
+emailField.innerHTML += "<span>Email</span><img src="https://sunbirdapp.com/icons/email.svg" alt="Email" />";
+countryField.innerHTML = "<div class="country__img"></div><span>Country</span><span class="placeholder">Please select...</span><div class="country-list"><div class="country-list__search"><img src="icons/search.svg" alt="Search" /><input type="text" placeholder="Primamy Label" /></div><ul>
+<li class="country-list-card">
+	<img
+		src="icons/flags/afganistan.svg"
+		alt="afganistan"
+	/><span>Afganistan</span>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li>
+<li class="country-list-card">
+	<img src="icons/flags/albania.svg" alt="albania" /><span
+		>Albania</span
+	>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li>
+<li class="country-list-card">
+	<img src="icons/flags/algeria.svg" alt="algeria" /><span
+		>Algeria</span
+	>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li>
+<li class="country-list-card">
+	<img src="icons/flags/andorra.svg" alt="andorra" /><span
+		>Andorra</span
+	>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li>
+<li class="country-list-card">
+	<img src="icons/flags/angola.svg" alt="angola" /><span
+		>Angola</span
+	>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li>
+<li class="country-list-card">
+	<img
+		src="icons/flags/argentina.svg"
+		alt="argentina"
+	/><span>Argentina</span>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li>
+<li class="country-list-card">
+	<img src="icons/flags/tunisia.svg" alt="tunisia" /><span
+		>Tunisia</span
+	>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li>
+<li class="country-list-card">
+	<img
+		src="icons/flags/united-arab-emirates.svg"
+		alt="united arab emirates"
+	/><span>United Arab Emirates</span>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li>
+<li class="country-list-card">
+	<img
+		src="icons/flags/united-kingdom.svg"
+		alt="united kingdom"
+	/><span>United Kingdom</span>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li>
+<li class="country-list-card">
+	<img
+		src="icons/flags/united-states.svg"
+		alt="united states"
+	/><span>United States</span>
+	<img
+		class="checkmark"
+		src="icons/checkmark.svg"
+		alt="checkmark"
+	/>
+</li></ul></div>
+<div class="country-btn"></div>
+<img class="chevron" src="icons/chevron.svg" alt="arrow-down" />";
 
 	
 firstName.children[1].style.cssText = 'pointer-events: none;position: absolute;left: 51px;top: 23px;-webkit-transition: 0.2s;-o-transition: 0.2s;transition: 0.2s;-webkit-transition-timing-function: ease;-o-transition-timing-function: ease;transition-timing-function: ease;-webkit-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);-o-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);font-weight: 600;font-size: 16px;line-height: 15px;color: #9cabc7;-webkit-transition: 0.2s all ease-out;-o-transition: 0.2s all ease-out;transition: 0.2s all ease-out';	
