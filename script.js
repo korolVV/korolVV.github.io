f0c485224a95efbeb88507460b90276fce9a9427
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-09 12:09:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,50 @@ window.addEventListener("DOMContentLoaded", function () {
 _document = window.parent.document;
 let nameField = _document.querySelector('.name>span');
 let emailField = _document.querySelector('.email .form-item');
+	
+nameField.style.cssText = '
+	pointer-events: none;
+	position: absolute;
+	left: 47px;
+	top: 21px;
+	-webkit-transition: 0.2s;
+	-o-transition: 0.2s;
+	transition: 0.2s;
+	-webkit-transition-timing-function: ease;
+	-o-transition-timing-function: ease;
+	transition-timing-function: ease;
+	-webkit-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
+	-o-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
+	transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
+	font-weight: 600;
+	font-size: 16px;
+	line-height: 15px;
+	color: #9cabc7;
+	-webkit-transition: 0.2s all ease-out;
+	-o-transition: 0.2s all ease-out;
+	transition: 0.2s all ease-out';
+	
+emailField.style.cssText = '
+	pointer-events: none;
+	position: absolute;
+	left: 47px;
+	top: 21px;
+	-webkit-transition: 0.2s;
+	-o-transition: 0.2s;
+	transition: 0.2s;
+	-webkit-transition-timing-function: ease;
+	-o-transition-timing-function: ease;
+	transition-timing-function: ease;
+	-webkit-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
+	-o-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
+	transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
+	font-weight: 600;
+	font-size: 16px;
+	line-height: 15px;
+	color: #9cabc7;
+	-webkit-transition: 0.2s all ease-out;
+	-o-transition: 0.2s all ease-out;
+	transition: 0.2s all ease-out';
 
 let nameFieldChildren = nameField.children;
 	
