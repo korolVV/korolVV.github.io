06111fdc1e380a9c82ce6ac6cf44f2f164815f16
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-09 12:42:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ window.addEventListener("DOMContentLoaded", function () {
 	
 	
 _document = window.parent.document;
-let nameField = _document.querySelector('.name>span');
-let emailField = _document.querySelector('.email .form-item');
+const nameField = _document.querySelector('.name>span');
+const emailField = _document.querySelector('.email .form-item');
 
-let nameFieldChildren = nameField.children;
+const nameFieldChildren = nameField.children;
 	
-    
-nameFieldChildren[2].innerHTML += "<span>First name</span>";
-nameFieldChildren[3].innerHTML += "<span>Last name</span>";
+const firstName = nameFieldChildren[2];
+const lastName = nameFieldChildren[3];
+
+firstName.innerHTML += "<span>First name</span>";
+lastName.innerHTML += "<span>Last name</span>";
 emailField.innerHTML += "<span>Email</span>";
 
 	
-nameFieldChildren[2].style.cssText = 'pointer-events: none;position: absolute;left: 47px;top: 21px;-webkit-transition: 0.2s;-o-transition: 0.2s;transition: 0.2s;-webkit-transition-timing-function: ease;-o-transition-timing-function: ease;transition-timing-function: ease;-webkit-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);-o-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);font-weight: 600;font-size: 16px;line-height: 15px;color: #9cabc7;-webkit-transition: 0.2s all ease-out;-o-transition: 0.2s all ease-out;transition: 0.2s all ease-out';	
-nameFieldChildren[3].style.cssText = 'pointer-events: none;position: absolute;left: 47px;top: 21px;-webkit-transition: 0.2s;-o-transition: 0.2s;transition: 0.2s;-webkit-transition-timing-function: ease;-o-transition-timing-function: ease;transition-timing-function: ease;-webkit-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);-o-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);font-weight: 600;font-size: 16px;line-height: 15px;color: #9cabc7;-webkit-transition: 0.2s all ease-out;-o-transition: 0.2s all ease-out;transition: 0.2s all ease-out';
-	
+firstName[2].style.cssText = 'pointer-events: none;position: absolute;left: 47px;top: 21px;-webkit-transition: 0.2s;-o-transition: 0.2s;transition: 0.2s;-webkit-transition-timing-function: ease;-o-transition-timing-function: ease;transition-timing-function: ease;-webkit-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);-o-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);font-weight: 600;font-size: 16px;line-height: 15px;color: #9cabc7;-webkit-transition: 0.2s all ease-out;-o-transition: 0.2s all ease-out;transition: 0.2s all ease-out';	
+lastName[2].style.cssText = 'pointer-events: none;position: absolute;left: 47px;top: 21px;-webkit-transition: 0.2s;-o-transition: 0.2s;transition: 0.2s;-webkit-transition-timing-function: ease;-o-transition-timing-function: ease;transition-timing-function: ease;-webkit-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);-o-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);font-weight: 600;font-size: 16px;line-height: 15px;color: #9cabc7;-webkit-transition: 0.2s all ease-out;-o-transition: 0.2s all ease-out;transition: 0.2s all ease-out';
+emailField[2].style.cssText = 'pointer-events: none;position: absolute;left: 47px;top: 21px;-webkit-transition: 0.2s;-o-transition: 0.2s;transition: 0.2s;-webkit-transition-timing-function: ease;-o-transition-timing-function: ease;transition-timing-function: ease;-webkit-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);-o-transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);font-weight: 600;font-size: 16px;line-height: 15px;color: #9cabc7;-webkit-transition: 0.2s all ease-out;-o-transition: 0.2s all ease-out;transition: 0.2s all ease-out';	
 });
