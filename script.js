d8c58a15a7dcefaa1d89e2bb639692031aa9fa33
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-10 18:38:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -161,9 +161,6 @@ window.addEventListener("DOMContentLoaded", () => {
 	emailSpan.children[0].remove();
 	emailSpan.innerHTML += '<div class=\"error-message\"><img src=\"https://sunbirdapp.com/icons/disclaimer.svg\" alt=\"error message\" />Wrong email format</div>';
 	checkbox.innerHTML += '<div class=\"error-message\"><img src=\"https://sunbirdapp.com/icons/disclaimer.svg\" alt=\"error message\" />Required</div>';
-	
-	const iframe = _document.querySelector('.vs_widget_frame_vs');
-	console.log(iframe);
-	iframe.style.cssText = 'height: 100% !important;max-height: 850px !important';
+
 	
 });
