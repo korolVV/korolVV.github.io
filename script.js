7a7ed5c0b249e072221901c1a8f38f6d90ffcf5d
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-12 18:07:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,6 +44,32 @@ window.addEventListener("DOMContentLoaded", () => {
 	
 	
 	
+	const countryLi = _document.querySelectorAll(".country-list-card");
+
+	countryLi.forEach((item, i) => {
+		item.addEventListener("click", () => {
+			countryLi.forEach((items, index) => {
+				if (index !== i) {
+					items.children[2].classList.remove("checkmark-active");
+				}
+			});
+			item.children[2].classList.toggle("checkmark-active");
+			country.classList.add("country-active");
+			countryImg.classList.add("country-img-active");
+			country.children[5].classList.toggle("chevron-active");
+			placeholderCountry.classList.add("country-span-color-active");
+			placeholderChange.textContent = item.children[1].textContent;
+			placeholderChange.style.fontWeight = "600";
+			placeholderChange.style.fontSize = "16px";
+			placeholderChange.style.color = "#26344E";
+			setTimeout(function () {
+				country.children[3].classList.toggle("country-list-active");
+			}, 300);
+		});
+	});
+	
+	
+	
 	const inputName = _document.querySelector('#first_name');
 	console.log(inputName);
 	inputName.addEventListener('click', () => {
