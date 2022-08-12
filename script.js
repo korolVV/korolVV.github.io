3f45f38f3332a30e7ce4d7a24dac060acd236fcc
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-12 17:47:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
 	_document = window.parent.document;
 
-	const inputName = _document.querySelector('#name_field');
+	const inputName = _document.querySelector('#first_name');
 	console.log(inputName);
 	inputName.addEventListener('click', () => {
 		console.log('Hello');
