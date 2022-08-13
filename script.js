19a09834e5e8b28a2aa96706da3d00455e02f2f3
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-08-13 13:51:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -229,12 +229,12 @@ window.addEventListener("DOMContentLoaded", () => {
 			email.style.cssText= "border-color: #e74e4e";
 		}
 		if(countrySpan.classList.contains('warning')){
-			country.style.marginBottom = '36px';
 			countryWarning.style.display = 'flex';
-			setTimeout(function () {countryWarning.style.display = 'none'; country.style.marginBottom = '0';}, 2000);
 			countryImg.style.backgroundImage = 'url(https://sunbirdapp.com/icons/country-red.svg)';
 			placeholderCountry.style.cssText = "color: #e74e4e";
 			country.style.cssText= "border-color: #e74e4e";
+			country.style.cssText = 'margin-bottom: 36px';
+			setTimeout(function () {countryWarning.style.display = 'none'; country.style.cssText = 'margin-bottom: 0';}, 2000);
 		} 
 		if(checkbox.classList.contains('warning')){
 			checkboxWarning.style.display = 'flex';
