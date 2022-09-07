7a255e643306bc622436a410ee82009a1dc6ab72
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2022-09-07 13:08:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -351,7 +351,7 @@ window.addEventListener("DOMContentLoaded", () => {
 	
 
 	const closeToggle = _document.querySelector('.lightbox_close_toggle');
-	const modalWindow = _document.querySelector('.vs_lb_wrapper_115839');
+	const modalWindow = _document.querySelector('#vs_lb_wrapper_115839');
 	
 	closeToggle.addEventListener('click', () => {
 		console.log(modalWindow);
